8026b87e2485c335d56774070d7b6b0bfaf9098d
Subhendra Basu <subhendra@able.do>
2022-07-22 10:09:53 +0530

    Trying to add API Calls

--- app/src/main/java/com/example/potd_21_july/MainActivity.kt
+++ app/src/main/java/com/example/potd_21_july/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
@@ -14,12 +13,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.potd_21_july.ui.theme.Potd_21_julyTheme
 import java.text.SimpleDateFormat
+import java.time.Duration
 import java.time.LocalDate
 import java.util.*
 
-
 class MainActivity : ComponentActivity() {
-
+    private val client = OkHttpClient()
+    val sdf = SimpleDateFormat("dd/MM/yyyy")
+    val currentDate = sdf.format(Date())
+    var date1 = LocalDate.parse(currentDate)
+    var date2 = date1.minus(Duration.ofHours(7))
+    val url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=\""+date2+"\"&end_date=\""+date1+"\""
+    val request = Request.Builder()
+        .url(url)
+        .build()
+    client.newCall(request).enqueue(object : Callback {
+        override fun onFailure(call: Call, e: IOException) {}
+        override fun onResponse(call: Call, response: Response) = println(response.body()?.string())
+    })
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -29,37 +40,26 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    val sdf = SimpleDateFormat("dd/MM/yyyy")
-                    val currentDate = sdf.format(Date())
-                    var date1 = LocalDate.parse(currentDate)
-                    var year1 = date1.year
-                    var month1 = date1.month
-                    var day1 = date1.dayOfMonth
-                    val url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2017-07-14&end_date=2017-07-21"
-                    val browserIntent =
-                        Intent(Intent.ACTION_VIEW, Uri.parse(url))
-                    startActivity(browserIntent)
-                    println(browserIntent)
-                    DisplayGrid()
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun DisplayGrid() {
+/*@Composable
+fun DisplayGrid(b:Intent) {
     LazyRow(
 
     ) {
 
     }
-}
+}*/
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Potd_21_julyTheme {
-        DisplayGrid()
+
     }
 }
