b6d1f9bd25cf5b4a54190b7ced625aa0cb1793bb
Subhendra Basu <subhendra@able.do>
2022-07-22 07:36:44 +0530

    Adding date arithmetic to extract images for last 7 days

--- app/src/main/java/com/example/potd_21_july/MainActivity.kt
+++ app/src/main/java/com/example/potd_21_july/MainActivity.kt
@@ -13,6 +13,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.potd_21_july.ui.theme.Potd_21_julyTheme
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.*
 
 
 class MainActivity : ComponentActivity() {
@@ -26,10 +29,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    val sdf = SimpleDateFormat("dd/MM/yyyy")
+                    val currentDate = sdf.format(Date())
+                    var date1 = LocalDate.parse(currentDate)
+                    var year1 = date1.year
+                    var month1 = date1.month
+                    var day1 = date1.dayOfMonth
                     val url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2017-07-14&end_date=2017-07-21"
                     val browserIntent =
                         Intent(Intent.ACTION_VIEW, Uri.parse(url))
                     startActivity(browserIntent)
+                    println(browserIntent)
                     DisplayGrid()
                 }
             }
