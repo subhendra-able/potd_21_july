9b8abd4acec9ba12bb008816ce10dc319a0c5f0c
Subhendra Basu <subhendra@able.do>
2022-07-22 10:24:06 +0530

    Working on the Kotlin Retrofit REST API client

--- app/src/main/java/com/example/potd_21_july/MainActivity.kt
+++ app/src/main/java/com/example/potd_21_july/MainActivity.kt
@@ -18,19 +18,12 @@ import java.time.LocalDate
 import java.util.*
 
 class MainActivity : ComponentActivity() {
-    private val client = OkHttpClient()
     val sdf = SimpleDateFormat("dd/MM/yyyy")
     val currentDate = sdf.format(Date())
     var date1 = LocalDate.parse(currentDate)
     var date2 = date1.minus(Duration.ofHours(7))
     val url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=\""+date2+"\"&end_date=\""+date1+"\""
-    val request = Request.Builder()
-        .url(url)
-        .build()
-    client.newCall(request).enqueue(object : Callback {
-        override fun onFailure(call: Call, e: IOException) {}
-        override fun onResponse(call: Call, response: Response) = println(response.body()?.string())
-    })
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
